20e1a272ee3ebe0794c070d98fe1d357fb5c15b4
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2023-05-12 19:28:16 -0300

    Requisito 3 feito!

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(getCurrencies());
+  }
+
   render() {
+    const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <>
+        <label htmlFor="value-input">
+          Valor
+          <input
+            type="number"
+            data-testid="value-input"
+            id="value-input"
+          />
+        </label>
+        {currencies ? (
+          <label htmlFor="currency-input">
+            Moeda
+            <select
+              name="currency-input"
+              id="currency-input"
+              data-testid="currency-input"
+            >
+              {currencies.map((cur, index) => (
+                <option key={ index } value={ cur }>{cur}</option>
+              ))}
+            </select>
+          </label>
+        ) : ''}
+        <label htmlFor="description-input">
+          Descrição
+          <input
+            type="text"
+            id="description-input"
+            data-testid="description-input"
+          />
+        </label>
+        <label htmlFor="method">
+          Método de pagamento
+          <select
+            name="method"
+            id="method"
+            data-testid="method-input"
+          >
+            <option value="cash">Dinheiro</option>
+            <option value="credit-card">Cartão de crédito</option>
+            <option value="debit-card">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Categoria
+          <select
+            name="tag"
+            id="tag"
+            data-testid="tag-input"
+          >
+            <option value="food">Alimentação</option>
+            <option value="leisure">Lazer</option>
+            <option value="work">Trabalho</option>
+            <option value="transportation">Transporte</option>
+            <option value="health">Saúde</option>
+          </select>
+        </label>
+        <button type="button">Adicionar</button>
+      </>
     );
   }
 }
 
-export default WalletForm;
+const mapStateToProps = (globalState) => ({
+  currencies: globalState.wallet.currencies,
+});
+
+export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,21 @@
-import { SUBMIT_EMAIL, SUBMIT_EXPENSE } from './actionCreators';
+import { GET_CURRENCIES, SUBMIT_EMAIL, SUBMIT_EXPENSE } from './actionCreators';
 
 const submitEmail = (email) => ({
   type: SUBMIT_EMAIL,
   payload: email,
 });
 
-export { submitEmail };
+const listCurrencies = (currency) => ({
+  type: GET_CURRENCIES,
+  payload: currency,
+});
+
+const getCurrencies = () => async (dispatch) => {
+  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const data = await response.json();
+  delete data.USDT;
+  const currencies = Object.keys(data);
+  dispatch(listCurrencies(currencies));
+};
+
+export { submitEmail, getCurrencies };
