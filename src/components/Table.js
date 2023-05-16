d14b07a320d81e107a2c51138b580704ce571ddd
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2023-05-16 14:13:23 -0300

    Requisito 6 feito!

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <>
+        <h1>Descrição dos gastos</h1>
+        <table>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </table>
+      </>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (globalState) => ({
+  globalState,
+});
+
+export default connect(mapStateToProps)(Table);
